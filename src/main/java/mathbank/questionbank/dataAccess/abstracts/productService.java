6a4428c17ae2bd179bf9893a07f275a5cb854f64
Alperyilma <alperyilmaz.be@gmail.com>
2022-02-08 17:01:39 +0100

    Initial commit

---
+++ src/main/java/mathbank/questionbank/dataAccess/abstracts/productService.java
@@ -0,0 +1,6 @@
+package mathbank.questionbank.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface productService extends JpaRepository {
+}
---
+++ src/main/java/mathbank/questionbank/entities/conceretes/Product.java
@@ -0,0 +1,31 @@
+package mathbank.questionbank.entities.conceretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private int productId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "link_of_product")
+    private String linkOfProduct;
+
+
+
+
+
+}
